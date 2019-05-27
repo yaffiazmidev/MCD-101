d20bbd5fd7fe7aba716c54d54fd0f130b0115e8f
Azmi <yaffiazmi.dev@gmail.com>
2019-05-27 18:58:40 +0700

    create project

---
+++ MCD-101/MCD-101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCD-101
+//
+//  Created by yaffi azmi on 27/05/19.
+//  Copyright © 2019 yaffi azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
