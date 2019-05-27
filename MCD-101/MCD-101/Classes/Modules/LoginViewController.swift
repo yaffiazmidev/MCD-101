f2faebdf3b5be9cdaa152b4f43a9019f9a565160
Azmi <yaffiazmi.dev@gmail.com>
2019-05-28 05:36:13 +0700

    UI Auto Layout

---
+++ MCD-101/MCD-101/Classes/Modules/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  MCD-101
+//
+//  Created by yaffi azmi on 27/05/19.
+//  Copyright © 2019 yaffi azmi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
